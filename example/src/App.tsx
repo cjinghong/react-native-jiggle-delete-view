8d9a358ba79064369408acbaa414e8f947432c6a
Chan Jing Hong <jinghongchan@gmail.com>
2021-02-20 12:27:34 +0800

    Added fade out animation. Added example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,140 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import JiggleDeleteView from 'react-native-jiggle-delete-view';
 
+type Fruit = {
+  name: string;
+  color: string;
+};
+const INITIAL_ITEMS: Fruit[] = [
+  { name: 'Apple', color: '#DB282C' },
+  { name: 'Orange', color: '#FFB238' },
+  { name: 'Banana', color: '#FFDA87' },
+  { name: 'Durian', color: '#B6E04B' },
+  { name: 'Grapes', color: '#1939B7' },
+  { name: 'Strawberry', color: '#FF7F69' },
+];
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [deleting, setDeleting] = React.useState(false);
+  const [items, setItems] = React.useState([...INITIAL_ITEMS]);
+
+  const deleteItem = (index: number) => {
+    items.splice(index, 1);
+    setItems([...items]);
+  };
+
+  const resetExample = () => {
+    setItems([...INITIAL_ITEMS]);
+  };
 
   React.useEffect(() => {
-    JiggleDeleteView.multiply(3, 7).then(setResult);
-  }, []);
+    if (!items.length) {
+      setDeleting(false);
+    }
+  }, [items]);
+
+  const renderItem = ({ item, index }: { item: Fruit; index: number }) => {
+    return (
+      <TouchableOpacity
+        onLongPress={() => {
+          setDeleting(!deleting);
+        }}
+        style={styles.cellContainer}
+      >
+        <JiggleDeleteView
+          deleting={deleting}
+          onDelete={() => {
+            deleteItem(index);
+          }}
+        >
+          <View style={[styles.cell, { backgroundColor: item.color }]}>
+            <Text style={styles.text}>{item.name}</Text>
+          </View>
+        </JiggleDeleteView>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        <FlatList
+          data={items}
+          renderItem={renderItem}
+          contentContainerStyle={styles.flatListContainer}
+          keyExtractor={(item) => item.name}
+          ListHeaderComponent={
+            <>
+              <Text style={styles.subtitle}>
+                Long press on any of the views to enter "Jiggle Mode". Long
+                press on the apps again to exit "Jiggle Mode."
+              </Text>
+              <Text style={styles.subtitle}>
+                You could also exit through your own button, just set "deleting"
+                to "false" to exit.
+              </Text>
+            </>
+          }
+          ListEmptyComponent={
+            <View style={styles.emptyContainer}>
+              <TouchableOpacity onPress={resetExample}>
+                <Text style={styles.emptyText}>Reset</Text>
+              </TouchableOpacity>
+            </View>
+          }
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 8,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#222B45',
+    textAlign: 'center',
+    paddingBottom: 16,
+  },
+  flatListContainer: {
+    flexGrow: 1,
+  },
+  cellContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+  },
+  cell: {
+    padding: 32,
+    borderRadius: 10,
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  emptyContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  emptyText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#10C6CD',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,127 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
+import React, { useEffect, useRef, useState } from 'react';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+interface DeletableViewProps {
+  children: JSX.Element;
+  deleting: Boolean;
+  onDelete: () => void;
+  disableDeletingAnimation?: boolean;
+}
+
+const deleteZoomoutAnimDuration = 300;
+const jiggle: any = {
+  0: {
+    rotate: '0deg',
+  },
+  0.25: {
+    rotate: '1.5deg',
+  },
+  0.75: {
+    rotate: '-1.5deg',
+  },
+  1: {
+    rotate: '0deg',
   },
 };
+
+// Tap and hold to enter delete mode
+const JiggleDeleteView: React.FC<DeletableViewProps> = ({
+  children,
+  deleting,
+  onDelete,
+  disableDeletingAnimation,
+}) => {
+  const [delay] = useState((Math.random() + 1) * 10);
+  const [nonce, setNonce] = useState(0);
+  const [deleted, setIsDeleted] = useState(false);
+
+  const viewRef = useRef<any>();
+
+  useEffect(() => {
+    if (!deleting) {
+      viewRef.current?.stopAnimation();
+    } else {
+      setNonce(nonce + 1);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deleting]);
+
+  const onDeletePress = () => {
+    // If delete animation disabled, just call onDelete
+    if (disableDeletingAnimation) {
+      onDelete();
+      return;
+    }
+
+    setIsDeleted(true);
+    viewRef.current?.animate('zoomOut').then(() => {
+      setTimeout(() => {
+        onDelete();
+      }, deleteZoomoutAnimDuration - 200);
+    });
+  };
+
+  return (
+    <View>
+      {deleting && !deleted && (
+        <TouchableOpacity
+          onPress={onDeletePress}
+          style={styles.deleteButtonContainer}
+          hitSlop={{ top: 3, right: 3, bottom: 3, left: 3 }}
+        >
+          <Animatable.View
+            style={[styles.deleteButton]}
+            animation="fadeIn"
+            duration={500}
+            useNativeDriver
+          >
+            <Image
+              source={{
+                uri:
+                  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAYAAABV7bNHAAAABmJLR0QA/wD/AP+gvaeTAAAAR0lEQVR4nO3QQQ4AEBAEQfz/z3xAMi6IqDrvYbZLAQAAAAAAAAAA+FRdvOtbV9wT/28nVrxMoECgQKBAIAAAAAAAAAAAAOYG9IIBDHhKZI8AAAAASUVORK5CYII=',
+              }}
+              style={styles.deleteIcon}
+            />
+          </Animatable.View>
+        </TouchableOpacity>
+      )}
+      <Animatable.View
+        key={nonce}
+        ref={viewRef}
+        animation={deleted ? 'zoomOut' : jiggle}
+        easing={deleted ? 'ease' : 'linear'}
+        duration={deleted ? deleteZoomoutAnimDuration : 280 + delay}
+        delay={deleted ? 0 : delay}
+        iterationCount={deleted ? 1 : 'infinite'}
+        useNativeDriver
+      >
+        {children}
+      </Animatable.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  deleteButtonContainer: {
+    zIndex: 999,
+  },
+  deleteButton: {
+    backgroundColor: '#FF4E44',
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    marginBottom: -13,
+    marginLeft: -6,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  deleteIcon: {
+    width: 16,
+    height: 16,
+    tintColor: 'white',
+  },
+});
+
+export default JiggleDeleteView;
